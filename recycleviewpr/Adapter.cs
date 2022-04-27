2fd9321f696927870b201f25485eeff9fb7097b6
aeris.ahir <aeris.ahir@1R-75>
2022-04-27 13:50:00 +0530

    Add project files.

---
+++ recycleviewpr/Adapter.cs
@@ -0,0 +1,78 @@
+﻿
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace recycleviewpr
+{
+    internal class Adapter : RecyclerView.Adapter
+    {
+        public event EventHandler<AdapterClickEventArgs> ItemClick;
+        public event EventHandler<AdapterClickEventArgs> ItemLongClick;
+        List<datamodal> list;
+        Context context;
+
+        public Adapter(List<datamodal> data, Context context)
+        {
+            list = data;
+            this.context = context;
+        }
+
+        // Create new views (invoked by the layout manager)
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+
+            //Setup your layout here
+            View itemView = null;
+            var id = Resource.Layout.layout1;
+            itemView = LayoutInflater.From(parent.Context).
+                 Inflate(id, parent, false);
+
+            var vh = new AdapterViewHolder(itemView, OnClick, OnLongClick);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        public override void OnBindViewHolder(RecyclerView.ViewHolder viewHolder, int position)
+        {
+            var item = list[position];
+
+            // Replace the contents of the view with that element
+            var holder = viewHolder as AdapterViewHolder;
+            //holder.TextView.Text = items[position];
+            holder.CheckBox.Text = item.videoss;
+           
+        }
+
+        public override int ItemCount => list.Count;
+
+        void OnClick(AdapterClickEventArgs args) => ItemClick?.Invoke(this, args);
+        void OnLongClick(AdapterClickEventArgs args) => ItemLongClick?.Invoke(this, args);
+
+    }
+
+    public class AdapterViewHolder : RecyclerView.ViewHolder
+    {
+        //public TextView TextView { get; set; }
+        public CheckBox CheckBox;
+
+
+        public AdapterViewHolder(View itemView, Action<AdapterClickEventArgs> clickListener,
+                            Action<AdapterClickEventArgs> longClickListener) : base(itemView)
+        {
+            //TextView = v;
+            CheckBox = itemView.FindViewById<CheckBox>(Resource.Id.checkBox1);
+            itemView.Click += (sender, e) => clickListener(new AdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+            itemView.LongClick += (sender, e) => longClickListener(new AdapterClickEventArgs { View = itemView, Position = AdapterPosition });
+        }
+    }
+
+    public class AdapterClickEventArgs : EventArgs
+    {
+        public View View { get; set; }
+        public int Position { get; set; }
+    }
+}
---
+++ recycleviewpr/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using System;
+using System.Collections.Generic;
+
+namespace recycleviewpr
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+       
+        private List<datamodal> list;
+        private RecyclerView recyclerView;
+        private RecyclerView.LayoutManager layoutManager;
+        private Adapter adapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView1);
+            addingdata();
+            layoutManager = new LinearLayoutManager(this, LinearLayoutManager.Vertical, true);
+            recyclerView.SetLayoutManager(layoutManager);
+            adapter = new Adapter(list, this);
+            recyclerView.SetAdapter(adapter);
+
+        }
+
+        private void addingdata()
+        {
+            list = new List<datamodal>
+            {    new datamodal{videoss="Dengue Fever"}, new datamodal{videoss="Malaria"},
+            new datamodal{videoss="Pneumonia "},new datamodal{videoss="Diarrhea "},
+             new datamodal{videoss="High Blood Pressure/ Hypertension "},new datamodal{videoss="Common Cold & Fever "},
+              new datamodal{videoss="Jaundice  "},new datamodal{videoss="Tuberculosis  "},
+               new datamodal{videoss="Depression   "},
+            };
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
